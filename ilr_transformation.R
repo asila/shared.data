c6d785b05f23ba0c9dd403b3e7ab7cb57b26aeb4
Andrew Sila <asila.cgiar.org>
2015-01-16 11:53:23 +0300

    modified ilr_transformations.R

--- ilr_transformation.R
+++ ilr_transformation.R
@@ -1,10 +1,15 @@
+#1:This is a small R-script to read compositional data for isometric log-ratio transformation.
+#2: A csv file with test data is available  in the shared dropbox and will be downloaded automatically
+#3: For the script to work correct you will need two packages: compositions and downloader if missing install them first
+#4: For assitance contact me at asila@cgiar.org
+
 #load required library
 library(compositions)
-#setwd where compositional data is
-#setwd("~/Dropbox/compositional.data")
-
+library(downloader)
+#download file from compositional dropbox folder and read it before ilr
+download("https://www.dropbox.com/s/qlpjef9q5krbz28/compositional_data.csv?dl=0","compositional_data.csv")
 #read data  sample XRD data
-xrd<-read.csv("~compositional data.csv")
+xrd<-read.csv("compositional_data.csv")
 
 #create compositional data prior to taking ilr  
 xrd.ac<-acomp(xrd.a[,-1])#Remove  the first column containing sample id
